2ae706931dd2530c4257a9bcca6a20dc756881fd
lbross@pdx.edu <lbross@pdx.edu>
2014-07-09 23:25:01 +0000

    Move logos image to resource file
    git-svn-id: http://Marc-PC/svn/Full@1928 85f15e0e-5137-b144-a863-98bd542365a3

--- tools/Widgets/branches/lbross/src/Launcher/Launcher.Designer.cs
+++ tools/Widgets/branches/lbross/src/Launcher/Launcher.Designer.cs
@@ -183,7 +183,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Image = global::Launcher.Properties.Resources.logos1;
             this.pictureBox1.Location = new System.Drawing.Point(491, 109);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(281, 249);
